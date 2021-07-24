db4faeed61118f9030a8a4fd4fedce6fa98f7d6b
erikyryan <62263916+erikyryan@users.noreply.github.com>
2021-07-24 14:29:09 -0300

    Modificações no menu de Pessoa

---
+++ src/menu/MenuVeiculo.java
@@ -0,0 +1,10 @@
+package menu;
+
+import java.util.Scanner;
+
+public interface MenuVeiculo {
+    public void remover(Scanner entrada);
+    public void exibir(Scanner entrada);
+    public void adicionar(Scanner entrada);
+    public void verificar(Scanner entrada);
+}
---
+++ src/menu/MenuVeiculoImpl.java
@@ -0,0 +1,31 @@
+package menu;
+
+import java.util.Scanner;
+
+public class MenuVeiculoImpl implements MenuVeiculo{
+
+	@Override
+	public void remover(Scanner entrada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void exibir(Scanner entrada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void adicionar(Scanner entrada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void verificar(Scanner entrada) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
