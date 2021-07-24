69e31a52710e37064c77894414cb2e08ba1b8f6b
erikyryan <62263916+erikyryan@users.noreply.github.com>
2021-07-24 14:29:54 -0300

    Adicionando o tratamento de exceção

--- src/App.java
+++ src/App.java
@@ -1,23 +1,32 @@
   
 import java.util.HashMap;
+import java.util.InputMismatchException;
 import java.util.Map;
 import java.util.Scanner;
 
-import api.LoginApplication;
 import api.MenuApplication;
 import entity.pessoa.PessoaFisica;
-import entity.pessoa.Status;
 
 public class App {
 	public static void main(String[] args) throws Exception {
-	    Map<String,PessoaFisica> pessoas = new HashMap<String,PessoaFisica>();
 	    Scanner entrada = new Scanner(System.in);
-	    MenuApplication menu = new MenuApplication();
-	    LoginApplication login = new LoginApplication();
-	    PessoaFisica p = new PessoaFisica(2,"ryan","1",Status.GERENTE);
-	    menu.MenuUsuario(entrada, pessoas);
-	    login.validacao(entrada,pessoas,p);
-	    entrada.close();
-	}
+		
+	    try {
+			Map<String,PessoaFisica> pessoas = new HashMap<String,PessoaFisica>();
+		    MenuApplication menu = new MenuApplication();
+	    
+		    menu.MenuUsuario(entrada, pessoas);
+		}catch(InputMismatchException e) {
+			System.err.println("[ERROR]: Falha ao passar os dados para o scanner");			
+			e.printStackTrace();
+		}catch(Exception e) {
+			e.printStackTrace(); // imprime o erro da excecao em formato de pilha
+		}finally {
+			entrada.close();
+		}
+		
+	    
+	
+		}
     
 }
