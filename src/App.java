4e5c5c5cebc4eb8a4c72a60e76fa30832d026de6
erikyryan <erikyryang@gmail.com>
2021-08-21 09:52:12 -0300

    Adicionando javaFX

--- src/App.java
+++ src/App.java
@@ -1,30 +1,33 @@
-  
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
 
-import api.MenuApplication;
-import entity.pessoa.PessoaFisica;
-import entity.pessoa.PessoaJuridica;
+import javafx.application.Application;
+import javafx.stage.Stage;
 
-public class App {
-	public static void main(String[] args) throws Exception {
-	    Scanner entrada = new Scanner(System.in);
-		
-	    try {
-			Map<String,PessoaFisica> pessoasFisicas = new HashMap<String,PessoaFisica>();
-			Map<String,PessoaJuridica> pessoasJuridicas = new HashMap<String,PessoaJuridica>();
-		    MenuApplication menu = new MenuApplication();
-		    
-		    menu.MenuUsuario( pessoasFisicas,pessoasJuridicas, entrada);
-		    
-		}catch(Exception e) {
-			System.err.println(e.toString());			
-			e.printStackTrace();
-		}finally {
-			entrada.close();
-		}
+public class App extends Application{
+	
+	@Override
+	public void start(Stage arg0) throws Exception {
 		
 	}
-    
+	
+	public static void main(String[] args) throws Exception {
+		
+	}
+
 }
+
+//	    Scanner entrada = new Scanner(System.in);
+
+//	    try {
+//			Map<String,PessoaFisica> pessoasFisicas = new HashMap<String,PessoaFisica>();
+//			Map<String,PessoaJuridica> pessoasJuridicas = new HashMap<String,PessoaJuridica>();
+//		    MenuApplication menu = new MenuApplication();
+//		    
+//		    menu.MenuUsuario( pessoasFisicas,pessoasJuridicas, entrada);
+//		    
+//		}catch(Exception e) {
+//			System.err.println(e.toString());			
+//			e.printStackTrace();
+//		}finally {
+//			entrada.close();
+//		}
+		
