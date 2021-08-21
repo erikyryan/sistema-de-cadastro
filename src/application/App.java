4e7738dbe1bc58e18b0ba79764ace0d5e9175176
erikyryan <erikyryang@gmail.com>
2021-08-21 10:44:55 -0300

    Tela de Teste

--- src/application/App.java
+++ src/application/App.java
@@ -1,17 +1,26 @@
 package application;
 
+import java.io.IOException;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class App extends Application{
 	
 	@Override
-	public void start(Stage arg0) throws Exception {
+	public void start(Stage primaryStage) throws IOException {
+		Pane root = FXMLLoader.load(getClass().getResource("Tela.fxml"));
 		
+		Scene scene = new Scene(root,300,300);
+		primaryStage.setScene(scene);
+		primaryStage.show();
 	}
 	
 	public static void main(String[] args) throws Exception {
-		
+		launch(args);
 	}
 
 }
