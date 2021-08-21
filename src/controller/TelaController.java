e9ae2475e59d0b7ef4240872baf3d850faf9e655
erikyryan <erikyryang@gmail.com>
2021-08-21 15:33:26 -0300

    Alterando TelaPessoaJuridica

--- src/controller/TelaController.java
+++ src/controller/TelaController.java
@@ -5,23 +5,16 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.App;
-import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
-import menu.MenuPessoasImpl;
 
 public class TelaController implements Initializable{
 
-	public static MenuPessoasImpl menuPessoas = new MenuPessoasImpl();
 	App app = new App();
 	
 	Stage stage = new Stage();
@@ -37,22 +30,22 @@ public class TelaController implements Initializable{
 
     @FXML
     void acaoPessoaFisica(ActionEvent event) throws IOException {
-    	
     	Scene scene = app.TelaPessoaFisica(stage);
-		
     	Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		stage.setScene(scene);
 		stage.show();
     }
 
 	@FXML
-    void acaoPessoaJuridica(ActionEvent event) {
-
+    void acaoPessoaJuridica(ActionEvent event) throws IOException {
+		Scene scene = app.TelaPessoaJuridica(stage);
+    	Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		stage.setScene(scene);
+		stage.show();
     }
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		// TODO Auto-generated method stub
 		
 	}
 }
