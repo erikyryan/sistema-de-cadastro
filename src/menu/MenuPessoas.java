c7c782aa5cf91c978371ceeb2150515f4858159b
erikyryan <erikyryang@gmail.com>
2021-08-24 09:32:34 -0300

    removendo menu desnecessario

--- src/menu/MenuPessoas.java
+++
@@ -1,9 +0,0 @@
-package menu;
-
-import entity.Sistema;
-
-public interface MenuPessoas {
-    public void remover(Sistema sistema, int user);
-    public void exibir(Sistema sistema, int user);
-    public void adicionar(Sistema sistema, int user);
-}
